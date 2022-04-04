2e3355c9420d7ac8c4d345ff447363adf374e59d
Omar Crespo <crespomerchano@gmail.com>
2022-04-04 18:49:24 -0500

    initial commit

---
+++ src/modules/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <header>
+        <h1>la vida es horrible</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
