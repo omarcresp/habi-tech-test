84bc9f08a8c337f5edecab027d18bfb163da283d
Omar Crespo <crespomerchano@gmail.com>
2022-04-05 00:02:37 -0500

    fixes for prod environment

--- src/modules/home/components/hero-start/index.tsx
+++ src/modules/home/components/hero-start/index.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from 'react';
 
+import iconAngleDown from 'assets/icons/angle-down.svg'
+
 import './index.css';
 
 const HeroStart: FC = () => (
   <a className="hero-start" href="#entryForm">
     <p className="hero-start__copy">Empezar</p>
 
-    <img className="hero-start__icon" src="/assets/icons/angle-down.svg" alt="see more icon" />
+    <img className="hero-start__icon" src={iconAngleDown} alt="see more icon" />
   </a>
 );
 
